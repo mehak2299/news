69b739567ba08404aca625acae068aedc7f63ece
mehak2299 <kaurkavneet72@gmail.com>
2024-11-01 14:57:19 +0530

    NewsClassComponent

---
+++ src/Compnents/New.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+export default class extends Component {
+  render() {
+    return (
+      <div>
+          <NewsItem/>
+          <NewsItem/>
+          <NewsItem/>
+      </div>
+    )
+  }
+}
